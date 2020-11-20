00137515d4f32dcf8d717927aad79ca5923a5661
anhtuan2797 <anhtuanug2797@gmail.com>
2020-11-20 22:43:19 +0700

    Finish topic recycler view

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.myapplication.TopicRecylerView.TopicRecylerViewAdapter.topicRecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> mTopicName = new ArrayList<>();
+    private ArrayList<String> mTopicThumb = new ArrayList<>();
+    private ArrayList<Boolean> mTopicDownloadStatus = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initImage();
+        initTopicRecyclerView();
     }
+
+    private void initImage(){
+        mTopicName.add("topic 1");
+        mTopicThumb.add("https://images.pexels.com/photos/5650027/pexels-photo-5650027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+        mTopicDownloadStatus.add(true);
+
+        mTopicName.add("topic 2");
+        mTopicThumb.add("https://images.pexels.com/photos/5650027/pexels-photo-5650027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+        mTopicDownloadStatus.add(false);
+
+        mTopicName.add("topic 3");
+        mTopicThumb.add("https://images.pexels.com/photos/5650027/pexels-photo-5650027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+        mTopicDownloadStatus.add(false);
+
+    }
+
+    private void initTopicRecyclerView(){
+        RecyclerView topicRecyclerView  = findViewById(R.id.topic_list_recycler_view);
+        topicRecyclerViewAdapter adapter = new topicRecyclerViewAdapter(mTopicName,mTopicThumb,mTopicDownloadStatus,this);
+        topicRecyclerView.setAdapter(adapter);
+        topicRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.i(null,"recycler create");
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/TopicRecylerView/TopicRecylerViewAdapter/topicRecyclerViewAdapter.java
@@ -0,0 +1,90 @@
+package com.example.myapplication.TopicRecylerView.TopicRecylerViewAdapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class topicRecyclerViewAdapter extends RecyclerView.Adapter<topicRecyclerViewAdapter.ViewHolder>{
+    private ArrayList<String> mTopicNames = new ArrayList<>();
+    private ArrayList<String> mTopicThumbs = new ArrayList<>();
+    private ArrayList<Boolean> mTopicDownloadStatus = new ArrayList<Boolean>();
+    private Context mContext;
+
+    public topicRecyclerViewAdapter(ArrayList<String> topicNames, ArrayList<String> topicThumbs, ArrayList<Boolean> topicDownloadStatus, Context context){
+        mTopicNames = topicNames;
+        mTopicThumbs = topicThumbs;
+        mTopicDownloadStatus = topicDownloadStatus;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.topic_card_view,parent,false);
+        return new ViewHolder(view);
+    }
+
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.i(null,"viewholder create bind");
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mTopicThumbs.get(position))
+                .into(holder.topic_thumb);
+        holder.topic_name.setText(mTopicNames.get(position));
+        if(mTopicDownloadStatus.get(position)){
+            holder.topic_download_status.setVisibility(View.VISIBLE);
+            Log.i(null,"true");
+        }
+        else {
+            holder.topic_download_status.setVisibility(View.INVISIBLE);
+            Log.i(null,"false");
+        }
+        holder.topic_card_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(null,"clicked");
+                Log.i(null,mTopicNames.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTopicNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView topic_name;
+        ImageView topic_thumb;
+        ImageView topic_download_status;
+        CardView topic_card_view;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Log.i(null,"view create");
+            topic_card_view = itemView.findViewById(R.id.topic_card_view);
+            topic_name = itemView.findViewById(R.id.topic_name);
+            topic_thumb = itemView.findViewById(R.id.topic_thumbnail);
+            topic_download_status = itemView.findViewById(R.id.topic_download_status);
+        }
+    }
+}
